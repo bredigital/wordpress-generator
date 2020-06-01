8fa98de7d795345d94869cbe7826b4a50b1dd3a7
soup-bowl <code@soupbowl.io>
2020-06-01 17:24:16 +0100

    Indicator for submissions.

--- src/assets/home.js
+++ src/assets/home.js
@@ -1,11 +1,25 @@
 window.onload = function () {
 	$newmsg = 'WARNING: All sites generated begin with a 60 day expiry. It is your responsibility to ensure the site remains active as long as it is needed, otherwise it will be automatically pruned.';
-	document.getElementById("btnCreateNew").onclick = function (e) {
-		return confirm($newmsg);
+	document.getElementById('btnCreateNew').onclick = function (e) {
+		var r = confirm($newmsg);
+		if (r == true) {
+			document.getElementById('btnCreateNew').style.display = 'none';
+			document.getElementById('btnCreateNewDisabled').style.display = '';
+			document.getElementById('btnImportNew').disabled = true;
+		}
+
+		return r;
 	}
 	
-	document.getElementById("btnImportNew").onclick = function (e) {
-		return confirm($newmsg);
+	document.getElementById('btnImportNew').onclick = function (e) {
+		var r = confirm($newmsg);
+		if (r == true) {
+			document.getElementById('btnImportNew').style.display = 'none';
+			document.getElementById('btnImportNewDisabled').style.display = '';
+			document.getElementById('btnCreateNew').disabled = true;
+		}
+
+		return r;
 	}
 
 	document.getElementById('uplBackup').addEventListener('change', function (e) {
